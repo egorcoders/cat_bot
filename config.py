e297c4e569c8a767de564578fc99a754150d8eca
Egor Boev <egorcoders@gmail.com>
2022-12-04 15:21:40 +0500

    #1 cat_bot

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = 'your_bot'  # Токен бота
+SEND_POLL = f'https://api.telegram.org/bot{TOKEN}/sendPoll'
+GET_UPDATES = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
+TIME_ZONE = 'Asia/Yekaterinburg'
+
+CAT_BOT_TIME = '08:00:00'  # Время отправки кошки
+CAT_URL = 'https://api.thecatapi.com/v1/images/search'
+
+TEST_DELAY = 10  # Ожидание до следующего опроса в секундах
+TEST_CHAT_ID = 'your_chat_id'
